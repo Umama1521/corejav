9fc56b8cd76dd670918e44c0345422ee5314af90
umama fatima <umamafatima1521@gmail.com>
2024-06-20 22:30:10 +0530

    commit message

---
+++ corejavaprogram/src/com/tns/encapsulation/Exce.java
@@ -0,0 +1,14 @@
+package com.tns.encapsulation;
+
+public class Exce {
+
+	public static void main(String[] args) {
+		Encapsulation e=new Encapsulation();
+		e.serialnumber=1;
+		e.name="umama";
+		e.age=20;
+		e.show(); //accessing the member function or method
+
+	}
+
+}
